cb3f98471b41a6e3accb0978054e2f7d221430b8
J <jakobdobias@web.de>
2023-10-30 15:23:17 +0100

    Version 1.1
    Version 1.1

--- src/me/jdot029/joinmessage/Main.java
+++ src/me/jdot029/joinmessage/Main.java
@@ -4,12 +4,11 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import me.jdot029.joinmessage.joinlistener.JoinListener;
 
-public class Main extends JavaPlugin{
+public class Main extends JavaPlugin {
 
-	
-	@Override
-	public void onEnable() {
-		new JoinListener(this);
-		
-	}
+    @Override
+    public void onEnable() {
+        getLogger().info("JoinMessage Plugin wurde aktiviert!");
+        new JoinListener(this);
+    }
 }
--- src/me/jdot029/joinmessage/joinlistener/JoinListener.java
+++ src/me/jdot029/joinmessage/joinlistener/JoinListener.java
@@ -6,23 +6,36 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 import me.jdot029.joinmessage.Main;
 
-public class JoinListener implements Listener{
-	
-	public JoinListener(Main plugin) {
-		Bukkit.getPluginManager().registerEvents(this, plugin);
-	}
-	@EventHandler(priority = EventPriority.HIGHEST)
-	public void onJoin(PlayerJoinEvent e) {
-		Player p = e.getPlayer();
-		
-		if (p.hasPlayedBefore() == true) {
-			e.setJoinMessage("§3Willkommen zurück§6 " + p.getName() + "§3!");
-		} else {
-			e.setJoinMessage("§3Willkommen auf diesem Server,§6 " + p.getName() + "§3!");
-		}
-	
-	}
+public class JoinListener implements Listener {
+
+    public JoinListener(Main plugin) {
+        Bukkit.getPluginManager().registerEvents(this, plugin);
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onJoin(PlayerJoinEvent e) {
+        Player p = e.getPlayer();
+
+        if (p.hasPlayedBefore()) {
+            e.setJoinMessage("§3Willkommen zurück§6 " + p.getName() + "§3!");
+            Bukkit.broadcastMessage("[§a+§r] §3" + p.getName());
+        } else {
+            e.setJoinMessage("§3Willkommen auf diesem Server,§6 " + p.getName() + "§3!");
+            // Broadcast Join-Nachricht nur für neue Spieler
+            Bukkit.broadcastMessage("§6§lWILLKOMMEN, " + p.getName() + " !");
+        }
+    }
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        event.setQuitMessage("[§c-§r] §3" + player.getName());
+
+        // Broadcast Quit-Nachricht an alle Spieler
+        Bukkit.broadcastMessage("[§c-§r] §c" + player.getName());
+    }
 }
